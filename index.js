7f7bcac228730f0c001f99c17fdab0c550df2838
amy-chinlai <amy@actionnetwork.org>
2021-12-20 16:12:50 -0500

    commit tree 3d1e075a4ed2242acece8dafc075829dd30006f4

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+
+// challenge 1
+function findMatching(drivers, args) {
+    return drivers.filter(d => d.toLowerCase() === args.toLowerCase())
+};
+
+// challenge 2
+function fuzzyMatch(drivers, letter) {
+    // return drivers.filter(d => d.charAt().toLowerCase() === letter.toLowerCase())
+    return drivers.filter(d => d.toLowerCase().indexOf(letter.toLowerCase()) === 0 )
+};
+
+// challenge 3
+function matchName(drivers, args) {
+    return drivers.filter(d => d.name === args)
+};
